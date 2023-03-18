b3b87246db6528f0aa47407bb2780a6f7d583d42
Inam Ul Huq <inambe.dev@gmail.com>
2023-03-18 22:31:40 +0500

    add stripe-utils

---
+++ src/api/stripe-utils/routes/stripe-utils.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/stripe-utils',
+    //  handler: 'stripe-utils.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
---
+++ src/api/stripe-utils/services/stripe-utils.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * stripe-utils service
+ */
+
+module.exports = () => ({});
