73e23366cd7c189b9ec0e79c981bb158bbab8417
Inam Ul Huq <inambe.dev@gmail.com>
2023-04-03 04:33:00 +0500

    add public download file route

--- src/api/song/controllers/song.js
+++ src/api/song/controllers/song.js
@@ -1,9 +1,20 @@
-'use strict';
-
+"use strict";
+const fs = require("fs");
 /**
  * song controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::song.song');
+module.exports = createCoreController("api::song.song", ({ strapi }) => ({
+  async download(ctx) {
+    ctx.query = { ...ctx.query, populate: "*" };
+    const response = await super.findOne(ctx);
+    const { title, mediaPreview } = response.data.attributes;
+
+    ctx.response.attachment(`${title}${mediaPreview.data.attributes.ext}`);
+    ctx.type = mediaPreview.data.attributes.mime;
+    ctx.body = fs.readFileSync(`public/${mediaPreview.data.attributes.url}`);
+    return response;
+  },
+}));
