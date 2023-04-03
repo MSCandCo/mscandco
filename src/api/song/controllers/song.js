a77b722c7ee84c0d74416981f33cff4f3a8d3dc1
Inam Ul Huq <inambe.dev@gmail.com>
2023-04-03 06:45:46 +0500

    a fix

--- src/api/song/controllers/song.js
+++ src/api/song/controllers/song.js
@@ -12,6 +12,9 @@ module.exports = createCoreController("api::song.song", ({ strapi }) => ({
     const response = await super.findOne(ctx);
     const { title, mediaPreview } = response.data.attributes;
 
+    // for allowing to access content-disposition on client side
+    ctx.set("Access-Control-Expose-Headers", "Content-Disposition");
+
     ctx.response.attachment(`${title}${mediaPreview.data.attributes.ext}`);
     ctx.type = mediaPreview.data.attributes.mime;
     ctx.body = fs.readFileSync(`public/${mediaPreview.data.attributes.url}`);
