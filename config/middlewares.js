fde2fc51019a75e2cfe7d24b59bac984bdfa6964
Inam Ul Huq <inambe.dev@gmail.com>
2024-04-23 19:06:22 +0500

    enable s3 in for uploading

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,37 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  { name: 'strapi::body', config: { includeUnparsed: true } },
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  { name: "strapi::body", config: { includeUnparsed: true } },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
