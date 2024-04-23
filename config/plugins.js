748ed960dba112948ddfaecdaa2a6d0680647352
Inam Ul Huq <inambe.dev@gmail.com>
2024-04-23 22:54:05 +0500

    Add CDN support

--- config/plugins.js
+++ config/plugins.js
@@ -3,6 +3,8 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
+        baseUrl: env("CDN_URL"),
+        rootPath: env("CDN_ROOT_PATH"),
         accessKeyId: env("AWS_ACCESS_KEY_ID"),
         secretAccessKey: env("AWS_ACCESS_SECRET"),
         region: env("AWS_REGION"),
